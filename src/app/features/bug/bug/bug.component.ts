47d020838b3bdad451181b483504784e48a8c426
D Terz <terzidis.dimi@gmail.com>
2020-01-31 13:41:47 +0200

    update Bug object added edit Bug update table

--- src/app/features/bug/bug/bug.component.ts
+++ src/app/features/bug/bug/bug.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Bug, BugService} from '../../../core/bug.service';
 import {ActivatedRoute, Router} from '@angular/router';
 
@@ -14,8 +14,11 @@ export class BugComponent implements OnInit {
   reporters = ['QA', 'PO', 'DEV'];
   statuses = ['Ready for Test', 'Done', 'Rejected'];
   priority = [{value: 1, display: 'Minor'}, {value: 2, display: 'Major'}, {value: 3, display: 'Critical'}];
+
   constructor(private bugService: BugService, private router: Router, private route: ActivatedRoute) { }
+
   public bug: Bug;
+
   ngOnInit() {
     this.form = new FormGroup({
       createdAt: new FormControl(new Date(), [Validators.required, Validators.minLength(3)]),
@@ -23,28 +26,44 @@ export class BugComponent implements OnInit {
       description: new FormControl(null, Validators.required),
       priority: new FormControl(null, Validators.required),
       reporter: new FormControl(null, Validators.required),
-      status: new FormControl(null, Validators.required)
+      status: new FormControl(null, Validators.required),
+      comments: new FormArray([this.createCommentFormGroup()])
     });
+
     const paramId = this.route.snapshot.params.id;
     if (paramId) {
       this.bugService.getBugById(paramId).subscribe(response => {
-        this.form = new FormGroup({
-          createdAt: new FormControl(response.createdAt, [Validators.required]),
-          title: new FormControl(response.title, [Validators.required, Validators.minLength(3)]),
-          description: new FormControl(response.description, Validators.required),
-          priority: new FormControl(response.priority, Validators.required),
-          reporter: new FormControl(response.reporter, Validators.required),
-          status: new FormControl(response.status, Validators.required)
-        });
+        this.form.patchValue({...response, comments: []});
+
+        if (response.comments) {
+          response.comments.forEach(comment => {
+            const commentFormGroup = this.createCommentFormGroup(comment);
+            this.commentArray.push((commentFormGroup))
+          })
+        }
       });
     }
   }
+
+  get commentArray(): FormArray {
+    return this.form.get('comments') as FormArray
+  }
+
+  private createCommentFormGroup(comment?) {
+    return new FormGroup({
+      reporter: new FormControl(comment && comment.reporter),
+      description: new FormControl(comment && comment.description)
+    })
+  }
+
   formSubmit(form: FormGroup) {
     this.bug = form.value;
-    this.bug.updatedAt = new Date();
-    this.bugService.pushBug(this.bug).subscribe(response => {
-      this.router.navigate(['']);
-    });
+    //TODO remove empty comments on create or update
+    const bugId = this.route.snapshot.params.id;
+    const actionToInvoke = bugId ? this.bugService.updateBug(bugId, this.bug) : this.bugService.pushBug(this.bug);
+    actionToInvoke.subscribe(response => {
+        this.router.navigate(['']);
+      });
   }
 
 }
