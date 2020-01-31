ddc233759769e61cf2971670afdab69bac4ff4be
D Terz <terzidis.dimi@gmail.com>
2020-01-31 21:33:55 +0200

    various fixes

--- src/app/features/bug-list/bug-list/bug-list.component.ts
+++ src/app/features/bug-list/bug-list/bug-list.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Bug, BugService} from '../../../core/bug.service';
 import {MatPaginator, MatSort, MatTableDataSource, PageEvent} from '@angular/material';
 import {Router} from '@angular/router';
 import {switchMap, tap} from 'rxjs/operators';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-bug-list',
@@ -12,20 +13,55 @@ import {switchMap, tap} from 'rxjs/operators';
 export class BugListComponent implements OnInit {
   displayedColumns: string[] = ['title', 'status', 'priority', 'reporter', 'createdAt', 'updatedAt','actions'];
 
+  @Input() dataLength: number; // TODO : issue#12 correct totalFeatures
+  @Input() dataPageIndex: number; // Which index is the current for the data paginator
+  @Input() dataPageSize: number = 5; // Size of paginator page data
+  @Input() pageSizeOptions: number[] = [5, 10, 25, 100]; // Size of paginator page data
+
+  @Output() pageChange: EventEmitter<PageEvent> = new EventEmitter();
+
   constructor(private bugService: BugService, private router: Router) { }
 
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  form: FormGroup;
+  priority = [{value: 1, display: 'Minor'}, {value: 2, display: 'Major'}, {value: 3, display: 'Critical'}];
   buglist: MatTableDataSource<Bug>;
   headers: string[];
-  pageNumber: number;
-  pageEvent: PageEvent;
-  datasource: null;
-  pageIndex:number;
-  pageSize:number;
-  length:number;
+  pageIndex: string;
+  pageSize: string;
+  filterValues:{
+    title: string;
+    priority: string;
+    reporter: string;
+    status: string;
+  };
 
+  ngOnInit() {
+    this.createFilterForm();
+    this.filterValues =this.form.value;
+    this.getAllBugs('','',this.dataPageSize.toString(),'','','','').subscribe();
+  }
 
-  applyFilter(filterValue: string) {
-    this.buglist.filter = filterValue.trim().toLowerCase();
+  applyFilter(form: FormGroup) {
+    this.filterValues = form.value;
+    return this.getAllBugs('','','5',this.filterValues.title,this.filterValues.priority,this.filterValues.reporter,this.filterValues.status).subscribe();
+  }
+
+  clearFilter(){
+    this.createFilterForm();
+    this.filterValues = this.form.value;
+    this.getAllBugs('','',this.dataPageSize.toString(),'','','','').subscribe();
+  }
+
+  private createFilterForm() {
+    this.form = new FormGroup({
+      title: new FormControl('',),
+      priority: new FormControl(''),
+      reporter: new FormControl(''),
+      status: new FormControl('')
+    });
   }
 
   getRecordForEdit(bugId: string) {
@@ -33,59 +69,28 @@ export class BugListComponent implements OnInit {
   }
 
   getServerData(event: PageEvent){
-      return this.bugService.getAll('',event.pageIndex.toString(),event.pageSize.toString(),'','','','').pipe(
-        tap((response: any) => {
-          // this.buglist = new MatTableDataSource(response);
-          const bug: any = response.body;
-          debugger;
-          const keys = response.headers.keys();
-          this.buglist = new MatTableDataSource(bug);
-          this.headers = keys.map(key =>
-            `${key}: ${response.headers.get(key)}`);
-          this.buglist.sort = this.sort;
-          this.buglist.paginator = this.paginator;
-        })
-      );
+    this.pageIndex =event.pageIndex.toString();
+    this.pageSize =event.pageSize.toString();
+    return this.getAllBugs('', this.pageIndex,this.pageSize,this.filterValues.title,this.filterValues.priority,this.filterValues.reporter,this.filterValues.status).subscribe();
   }
 
   deleteBug(bugId: string,event: MouseEvent){
     event.stopPropagation();
     this.bugService.deleteBug(bugId).pipe(
-      switchMap(() => this.getAllBugs())
+      switchMap(() => this.getAllBugs('','','','','','',''))
     ).subscribe();
   }
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-
-  ngOnInit() {
-    this.pageNumber=0;
-    this.getAllBugs().subscribe();
-  }
-
-  private getAllBugs() {
-    /*return this.bugService.getAllWithoutParams().pipe(
-      tap((response) => {
-        // this.buglist = new MatTableDataSource(response);
-        const bug: any = response.body;
-        const keys = response.headers.keys();
-        this.buglist = new MatTableDataSource(bug);
-        this.headers = keys.map(key =>
-          `${key}: ${response.headers.get(key)}`);
-        this.buglist.sort = this.sort;
-        this.buglist.paginator = this.paginator;
-      })
-    );*/
-    return this.bugService.getAll('','','','','','','').pipe(
+  private getAllBugs(sort: string, page: string,size: string,title: string,priority: string,reporter: string,status: string) {
+    return this.bugService.getAll(sort,page,size,title,priority,reporter,status).pipe(
       tap((response: any) => {
-        // this.buglist = new MatTableDataSource(response);
         const bug: any = response.body;
-        const keys = response.headers.keys();
         this.buglist = new MatTableDataSource(bug);
+        const keys = response.headers.keys();
         this.headers = keys.map(key =>
-          `${key}: ${response.headers.get(key)}`);
+          response.headers.get(key));
+        this.dataLength = +this.headers[5];
         this.buglist.sort = this.sort;
-        this.buglist.paginator = this.paginator;
       })
     );
   }
