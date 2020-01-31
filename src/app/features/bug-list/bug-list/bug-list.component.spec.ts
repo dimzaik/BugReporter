52c393daf2f08c4dc65c221065c720c15a5c29b8
dimzaik <dimzaik@gmail.com>
2020-01-31 17:22:27 +0200

    some tests with Karma and an Search.
    some tests with Karma and an Search.

--- src/app/features/bug-list/bug-list/bug-list.component.spec.ts
+++ src/app/features/bug-list/bug-list/bug-list.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { BugListComponent } from './bug-list.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatTableModule, MatSortModule, MatPaginatorModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 describe('BugListComponent', () => {
   let component: BugListComponent;
@@ -8,18 +16,34 @@ describe('BugListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BugListComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, CommonModule,
+        RouterModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientTestingModule],
+      declarations: [BugListComponent],
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BugListComponent);
-    component = fixture.componentInstance;
+
+
+  it('should the component be created', () => {
+    const fixture = TestBed.createComponent(BugListComponent);
+    const component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
+    //const compiled = fixture.debugElement.nativeElement;
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should Add button text be equal to Add new bug', () => {
+    const fixture = TestBed.createComponent(BugListComponent);
+    const component = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+    expect(component.querySelector('.mat-button').textContent).toEqual('Add new Bug');
   });
 });
